e55aaf6d98baaa9311f5dc206a39673b966e4fb8
Noufal S <noufal@docme.cloud>
2024-10-16 10:30:53 +0530

    company menu

--- application/mask/sidebar/new_headder.php
+++ application/mask/sidebar/new_headder.php
@@ -342,8 +342,13 @@ function mainMenu($encUserId)
                   <i class="side-menu__icon fa-solid fa-house"></i><span class="side-menu__label">Company</span></a>
     	<ul class="slide-menu open">';
     
-    $menuHtml .= '<li class="sub-slide-item">' . x(array('link' => 'erp_report/company/statement','label' => 'Monthly Statement',array("param" => 'wide_opener'))) . '</li>';
+    
+    
+    $menuHtml .= '<li class="sub-slide-item">' . x(array('link' => 'erp_manage/company/list','label' => 'Company Documents',array())) . '</li>';
         
+    $menuHtml .= '<li class="sub-slide-item">' . x(array('link' => 'erp_report/company/statement','label' => 'Monthly Statement',array("param" => 'wide_opener'))) . '</li>';
+    
+    
     $menuHtml .= '</ul>
       </li>';
     
